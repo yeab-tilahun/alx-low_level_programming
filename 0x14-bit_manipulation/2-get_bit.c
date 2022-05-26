07864ecf384003461ecf71f84c14265c058f5860
Yeabsira <yeabsiratilahun78@gmail.com>
2022-05-27 01:02:50 +0300

    Add files via upload

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - gets int to index number
+ * @n: containing the decimal number
+ * @index: containing the index
+ * Return: the converted number
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int status = (n >> index) & 1;
+if (index > 32)
+return (-1);
+return (status);
+}
